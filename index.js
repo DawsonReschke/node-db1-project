1ed9918ab0874131f2cedd146cebe047417a81f3
dawson <dawsonreschke@icloud.com>
2022-04-18 15:00:33 -0700

    completed all required tasks

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./api/server.js");
-
+const db = require('./api/accounts/accounts-model')
 const PORT = process.env.PORT || 9000;
+
+db.getAll().then(all=>console.log(all))
+
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
--- queries.sql
+++ queries.sql
@@ -1,13 +1,34 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-
+    CustomerID : [12,54,65]
 -- Find the phone number for the supplier with the id 11
-
+    (010) 9984510
 -- List first 10 orders placed, sorted descending by the order date
-
+OrderId: [
+    10443
+    10442
+    10440
+    10441
+    10439
+    10438
+    10436
+    10437
+    10435
+    10433
+]
 -- Find all customers that live in London, Madrid, or Brazil
-
+CustomerID: [
+    4
+    8
+    11
+    16
+    19
+    22
+    53
+    69
+    72
+]
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
